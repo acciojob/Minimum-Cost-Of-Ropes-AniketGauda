5e620fa4302a169ff6d7febd622b13d6fd0c80a9
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-29 08:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+let inp = document.getElementById("rope-lengths");
+
+let pDiv = document.getElementById("result");
+
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+    let str = inp.value;
+    let a = str.split(",");
+    
+    let arr = a.map((elt)=>{
+        return parseInt(elt);
+    })
+    
+    arr.sort((a,b)=>{
+        return a-b;
+    })
+
+    let sm=0;
+    while(arr.length>1){
+        let tmp = arr[0]+arr[1];
+        sm+=tmp;
+        arr.shift();arr.shift();
+        arr.unshift(tmp);
+
+        arr.sort((a,b)=>{
+            return a-b;
+        })
+    }
+    pDiv.textContent = `${sm}`;
 }  
+  
+
